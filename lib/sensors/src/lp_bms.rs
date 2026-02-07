fff4a7e1f884bc91298b418794e2dda52fb5f375
BL-CZY <jiguangllsw@gmail.com>
2026-02-07 21:11:32 +0000

    async

--- lib/sensors/src/lp_bms.rs
+++ lib/sensors/src/lp_bms.rs
@@ -1,12 +1,9 @@
-use core::task::Poll;
-
 /// Driver for the TinyBMS s516 30A Battery Management System using CAN.
 /// Used to monitor battery status and health.
 /// For now it is assumed that the BMS communicates with another bus
 use defmt::Format;
-use embassy_stm32::can::CanRx;
-use embassy_time::{Duration, Instant};
-use hyped_can::{CanError, HypedCanFrame, HypedCanRx, HypedCanTx};
+use embassy_stm32::can::{enums::BusError, frame::Header, CanRx, CanTx, Frame, Id, StandardId};
+use hyped_can::CanError;
 
 /// Represents the parsed status from the BMS.
 #[derive(Debug, PartialEq, Clone, Format)]
@@ -20,88 +17,99 @@ pub struct BatteryData {
 }
 
 pub struct Bms {
-    can: CanRx<'static>,
+    can_rx: CanRx<'static>,
+    can_tx: CanTx<'static>,
 }
 
-pub struct ReadResponseFuture<'a, T: HypedCanTx + HypedCanRx + 'a> {
-    can: &'a mut T,
-    response_id: u32,
-    expected_cmd: u8,
-    start: Instant,
-}
-
-impl<'a, T: HypedCanTx + HypedCanRx> core::future::Future for ReadResponseFuture<'a, T> {
-    type Output = Result<[u8; 8], CanError>;
-
-    fn poll(
-        self: core::pin::Pin<&mut Self>,
-        cx: &mut core::task::Context<'_>,
-    ) -> core::task::Poll<Self::Output> {
-        let this = self.get_mut();
-        let envelope = this.can.read_frame()?;
-        if envelope.frame.can_id == this.response_id && envelope.frame.data[1] == this.expected_cmd
-        {
-            Poll::Ready(Ok(envelope.frame.data))
-        } else {
-            let elapsed = Instant::now() - this.start;
-            if elapsed > Duration::from_millis(1000) {
-                return Poll::Ready(Err(CanError::Timeout));
-            }
-            cx.waker().wake_by_ref();
-            Poll::Pending
-        }
+fn to_can_error(e: BusError) -> CanError {
+    match e {
+        BusError::Crc => CanError::Crc,
+        BusError::Form => CanError::Form,
+        BusError::Stuff => CanError::Stuff,
+        BusError::BusOff => CanError::BusOff,
+        BusError::Software => CanError::Software,
+        BusError::Acknowledge => CanError::Acknowledge,
+        BusError::BitRecessive => CanError::BitRecessive,
+        BusError::BitDominant => CanError::BitDominant,
+        BusError::BusPassive => CanError::BusPassive,
+        BusError::BusWarning => CanError::BusWarning,
     }
 }
 
-pub const BMS_NODE_ID: u8 = 0x01;
-pub const BMS_REQUEST_ID: u32 = 0x400 | BMS_NODE_ID as u32;
-pub const BMS_RESPONSE_ID: u32 = 0x500 | BMS_NODE_ID as u32;
+impl Bms {
+    const NODE_ID: u8 = 0x01;
+    const REQUEST_ID: u32 = 0x400 | Self::NODE_ID as u32;
+    const RESPONSE_ID: u32 = 0x500 | Self::NODE_ID as u32;
 
-impl<'a, T: HypedCanTx + HypedCanRx> Bms<'a, T> {
-    const REQUEST_ID: u32 = 0x400 | BMS_NODE_ID as u32;
-    const RESPONSE_ID: u32 = 0x500 | BMS_NODE_ID as u32;
+    async fn send_simple_request(&mut self, cmd: u8) -> Result<(), CanError> {
+        self.can_tx
+            .write(
+                &Frame::new(
+                    Header::new(
+                        embassy_stm32::can::Id::Standard(
+                            StandardId::new(Self::REQUEST_ID as u16).ok_or(CanError::Unknown)?,
+                        ),
+                        0,
+                        false,
+                    ),
+                    &[cmd, 0, 0, 0, 0, 0, 0, 0],
+                )
+                .map_err(|_| CanError::Unknown)?,
+            )
+            .await;
 
-    fn send_simple_request(&mut self, cmd: u8) -> Result<(), CanError> {
-        let frame = HypedCanFrame::new(Self::REQUEST_ID, [cmd, 0, 0, 0, 0, 0, 0, 0]);
-        self.can.write_frame(&frame)
+        Ok(())
     }
 
     async fn read_response(&mut self, expected_cmd: u8) -> Result<[u8; 8], CanError> {
-        let future = ReadResponseFuture {
-            can: self.can,
-            response_id: Self::RESPONSE_ID,
-            expected_cmd,
-            start: Instant::now(),
+        let frame = loop {
+            let envelope = self.can_rx.read().await.map_err(to_can_error)?;
+            let id = envelope.frame.id();
+
+            if let Id::Standard(id) = id {
+                if id.as_raw() as u32 == Self::RESPONSE_ID {
+                    break envelope.frame;
+                }
+            }
         };
-        future.await
+
+        assert_eq!(frame.data()[0], expected_cmd);
+
+        let mut result = [0u8; 8];
+
+        for i in 0..frame.data().len() {
+            result[i] = frame.data()[i];
+        }
+
+        Ok(result)
     }
 
     pub async fn read_voltage(&mut self) -> Result<f32, CanError> {
-        self.send_simple_request(0x14)?;
+        self.send_simple_request(0x14).await?;
         let data = self.read_response(0x14).await?;
         Ok(f32::from_le_bytes([data[2], data[3], data[4], data[5]]))
     }
 
     pub async fn read_current(&mut self) -> Result<f32, CanError> {
-        self.send_simple_request(0x15)?;
+        self.send_simple_request(0x15).await?;
         let data = self.read_response(0x15).await?;
         Ok(f32::from_le_bytes([data[2], data[3], data[4], data[5]]))
     }
 
     pub async fn read_max_cell_voltage(&mut self) -> Result<u16, CanError> {
-        self.send_simple_request(0x16)?;
+        self.send_simple_request(0x16).await?;
         let data = self.read_response(0x16).await?;
         Ok(u16::from_le_bytes([data[2], data[3]]))
     }
 
     pub async fn read_min_cell_voltage(&mut self) -> Result<u16, CanError> {
-        self.send_simple_request(0x17)?;
+        self.send_simple_request(0x17).await?;
         let data = self.read_response(0x17).await?;
         Ok(u16::from_le_bytes([data[2], data[3]]))
     }
 
     pub async fn read_temperatures(&mut self) -> Result<[i16; 3], CanError> {
-        self.send_simple_request(0x1B)?;
+        self.send_simple_request(0x1B).await?;
         let mut temps = [0i16; 3];
         for temp in &mut temps {
             let data = self.read_response(0x1B).await?;
@@ -110,36 +118,31 @@ impl<'a, T: HypedCanTx + HypedCanRx> Bms<'a, T> {
         Ok(temps)
     }
 
-    pub fn read_cell_voltages(&mut self) -> Result<heapless::Vec<u16, 32>, CanError> {
-        self.send_simple_request(0x1C)?;
+    pub async fn read_cell_voltages(&mut self) -> Result<heapless::Vec<u16, 32>, CanError> {
+        self.send_simple_request(0x1C).await?;
         let mut voltages = heapless::Vec::<u16, 32>::new();
 
-        let start = Instant::now();
         loop {
-            if Instant::now() - start > Duration::from_millis(1000) {
-                return Err(CanError::Full);
-            }
-
-            match self.can.read_frame() {
-                Ok(envelope)
-                    if envelope.frame.can_id == Self::RESPONSE_ID
-                        && envelope.frame.data[1] == 0x1C =>
-                {
-                    let data = envelope.frame.data;
-                    let val = u16::from_le_bytes([data[2], data[3]]);
-                    voltages.push(val).map_err(|_| CanError::BufferOverflow)?;
+            match self.can_rx.read().await {
+                Ok(envelope) => {
+                    if let Id::Standard(id) = envelope.frame.id() {
+                        if id.as_raw() as u32 == Self::RESPONSE_ID
+                            && envelope.frame.data()[1] == 0x1C
+                        {
+                            let data = envelope.frame.data();
+                            let val = u16::from_le_bytes([data[2], data[3]]);
+                            voltages.push(val).map_err(|_| CanError::BufferOverflow)?;
+                        } else {
+                            break;
+                        }
+                    } else {
+                        break;
+                    }
                 }
-                Ok(_) => break,
-                Err(e) => return Err(e),
+                Err(e) => return Err(to_can_error(e)),
             }
         }
         Ok(voltages)
-    }
-}
-
-impl<'a, T: HypedCanTx + HypedCanRx> Bms<'a, T> {
-    pub fn new(can: &'a mut T) -> Self {
-        Bms { can }
     }
 
     pub async fn read_battery_data(&mut self) -> Result<BatteryData, CanError> {
@@ -148,7 +151,7 @@ impl<'a, T: HypedCanTx + HypedCanRx> Bms<'a, T> {
         let max_cell_mv = self.read_max_cell_voltage().await?;
         let min_cell_mv = self.read_min_cell_voltage().await?;
         let temperatures_c = self.read_temperatures().await?;
-        let cell_voltages_mv = self.read_cell_voltages()?;
+        let cell_voltages_mv = self.read_cell_voltages().await?;
 
         Ok(BatteryData {
             voltage,
@@ -159,8 +162,4 @@ impl<'a, T: HypedCanTx + HypedCanRx> Bms<'a, T> {
             cell_voltages_mv,
         })
     }
-}
-
-impl<'a, T: HypedCanTx + HypedCanRx> Bms<'a, T> {
-    pub fn init(&mut self) {}
 }
