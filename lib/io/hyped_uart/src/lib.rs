c31a14d7888eedd7dbe5b7109f724f3a642ec9ca
BL-CZY <jiguangllsw@gmail.com>
2026-02-26 11:14:10 +0000

    uart trait

--- lib/io/hyped_uart/src/lib.rs
+++ lib/io/hyped_uart/src/lib.rs
@@ -1,21 +1,43 @@
 #![no_std]
 
-pub trait HypedUart {
-    // Add required functions here
+use core::future::Future;
+
+pub enum UartErr {
+    Framing,
+    Noise,
+    Overrun,
+    Parity,
+    BufferTooLong,
 }
 
-pub mod mock_uart {
-    /// A mock UART instance which can be used for testing
-    pub struct MockUart {}
-
-    impl crate::HypedUart for MockUart {
-        // Add required functions here
-    }
-
-    impl MockUart {
-        #[allow(clippy::new_without_default)]
-        pub fn new() -> Self {
-            Self {}
-        }
-    }
+pub trait HypedUart: Sized {
+    fn write(buffer: &[u8]) -> impl Future<Output = Result<(), UartErr>> + Send;
+    fn read(buffer: &mut [u8]) -> impl Future<Output = Result<(), UartErr>> + Send;
+    fn flush() -> impl Future<Output = Result<(), UartErr>> + Send;
 }
+//
+// pub mod mock_uart {
+//     /// A mock UART instance which can be used for testing
+//     pub struct MockUart {}
+//
+//     impl crate::HypedUart for MockUart {
+//         async fn write(_data: &[u8]) -> Result<(), crate::UartErr> {
+//             todo!()
+//         }
+//
+//         async fn read(_buffer: &mut [u8]) -> Result<(), crate::UartErr> {
+//             todo!()
+//         }
+//
+//         async fn flush() -> Result<(), crate::UartErr> {
+//             todo!()
+//         }
+//     }
+//
+//     impl MockUart {
+//         #[allow(clippy::new_without_default)]
+//         pub fn new() -> Self {
+//             Self {}
+//         }
+//     }
+// }
