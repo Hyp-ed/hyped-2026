13276afabf78b9dbbf79c38ad29391f23def79ec
Harvey Allen <harveyjallen1@gmail.com>
2026-06-06 00:22:39 +0100

    Motor control board ready to go up to ReadyForPropulsion state. Event handling stuff not done and need to add in handling for accelerate and brake commands.

--- boards/stm32f767zi/src/motor_control/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod control_loop;
-pub mod braking;
-pub mod navigation;
-pub mod event_handling;
---
+++ boards/stm32f767zi/src/tasks/motor_control/receive.rs
@@ -0,0 +1,22 @@
+use embassy_stm32::can::CanRx;
+use defmt::*;
+
+#[embassy_executor::task]
+pub async fn motor_rx_task(mut rx: CanRx<'static>) {
+    loop {
+        match rx.read().await {
+            Ok(envelope) => {
+                let frame = envelope.frame;
+                let raw_id: u32 = match frame.id() {
+                    embassy_stm32::can::Id::Standard(id) => id.as_raw() as u32,
+                    embassy_stm32::can::Id::Extended(id) => id.as_raw(),
+                };
+                info!("Motor RX: id={=u32:x} data={=[u8]:#02x}", raw_id, frame.data());
+            }
+            Err(e) => {
+                warn!("Motor CAN receive error: {:?}", e);
+                continue;
+            }
+        }
+    }
+}
